19baac5e8e626f80188faaa8e36831490b3b7005
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-12 13:25:34 -0400

    Color, font, and sizing changes

--- src/secciones/Hero.tsx
+++ src/secciones/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <div className={styles.hero}>
             <div className={styles.heroFloat}>
-                <span className={styles.logo}>Vínculos</span>
+                <span className={`logo ${styles.logo}`}>Vínculos</span>
                 <h1 className={styles.title}>tu boda, <br/>perfectamente imaginada</h1>
                 <h3 className={styles.subtitle}>bodas de lujo</h3>
                 <Boton fill="main" className={styles.button}>Continuar</Boton>
