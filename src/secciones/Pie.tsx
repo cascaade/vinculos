ee7740d6c64dc8d39cebf5097e38af579b8dfa0f
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-11 22:03:10 -0400

    Style footer

--- src/secciones/Pie.tsx
+++ src/secciones/Pie.tsx
@@ -1,23 +1,27 @@
 import styles from './Pie.module.scss';
 
 export default function Pie() {
-    return (<>
+    return (<footer className={styles.footer}>
         <section className={styles.main}>
             <span className={`logo ${styles.logo}`}>Vínculos</span>
             <div className={styles.links}>
-                <a href="#invitaciones">invitaciones</a>
-                <a href="#ropa">ropa</a>
-                <a href="#comida">comida</a>
-                <br/>
-                <a href="#decoraciones">decoraciones</a>
-                <a href="#entretenimiento">entretenimiento</a>
-                <br/>
-                <a href="#negocio">negocio</a>
-                <a href="#contacto">contacto</a>
+                <div>
+                    <a href="#invitaciones">invitaciones</a>
+                    <a href="#ropa">ropa</a>
+                    <a href="#comida">comida</a>
+                </div>
+                <div>
+                    <a href="#entretenimiento">entretenimiento</a>
+                    <a href="#decoraciones">decoraciones</a>
+                </div>
+                <div>
+                    <a href="#negocio">negocio</a>
+                    <a href="#contacto">contacto</a>
+                </div>
             </div>
         </section>
         <section className={styles.copyright}>
             &copy; 2026 Owen. All Rights Reserved
         </section>
-    </>);
+    </footer>);
 }
