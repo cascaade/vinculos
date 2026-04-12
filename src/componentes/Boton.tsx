55f31aec7b29912e9b3839a491280e48aa463bdd
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-11 20:58:36 -0400

    Add button and hero content

--- src/componentes/Boton.tsx
+++ src/componentes/Boton.tsx
@@ -1,3 +1,15 @@
-export default function Boton() {
-    return (<></>);
+import type {ReactNode} from "react";
+import styles from './Boton.module.scss';
+
+export type BotonProps = {
+    children: ReactNode,
+    fill: "main" | "ghost",
+    href?: string,
+    onclick?: () => void,
+};
+
+export default function Boton({ children, fill, href, onclick }: BotonProps) {
+    return (
+        <a className={`${styles.boton} ${fill === "main" ? styles.main : styles.ghost}`} href={href} onClick={onclick}>{children}</a>
+    );
 }
--- src/secciones/Hero.tsx
+++ src/secciones/Hero.tsx
@@ -1,3 +1,14 @@
+import styles from './Hero.module.scss';
+import Boton from "../componentes/Boton.tsx";
+
 export default function Hero() {
-    return (<></>);
+    return (
+        <div className={styles.hero}>
+            <span className={styles.logo}>Vínculos</span>
+            <h1 className={styles.title}>tu boda, perfectamente imaginada</h1>
+            <h3>bodas de lujo</h3>
+            <Boton fill="main">Continuar</Boton>
+            <Boton fill="ghost">Contáctanos</Boton>
+        </div>
+    );
 }
