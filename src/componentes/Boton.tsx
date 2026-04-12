598f02efa9b8b58f80b5b56eeb0ca57e844a23a5
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-11 21:33:49 -0400

    Style hero

--- src/componentes/Boton.tsx
+++ src/componentes/Boton.tsx
@@ -4,12 +4,13 @@ import styles from './Boton.module.scss';
 export type BotonProps = {
     children: ReactNode,
     fill: "main" | "ghost",
+    className: string,
     href?: string,
     onclick?: () => void,
 };
 
-export default function Boton({ children, fill, href, onclick }: BotonProps) {
+export default function Boton({ children, fill, href, onclick, className }: BotonProps) {
     return (
-        <a className={`${styles.boton} ${fill === "main" ? styles.main : styles.ghost}`} href={href} onClick={onclick}>{children}</a>
+        <a className={`${className} ${styles.boton} ${fill === "main" ? styles.main : styles.ghost}`} href={href} onClick={onclick}>{children}</a>
     );
 }
--- src/secciones/Hero.tsx
+++ src/secciones/Hero.tsx
@@ -4,11 +4,13 @@ import Boton from "../componentes/Boton.tsx";
 export default function Hero() {
     return (
         <div className={styles.hero}>
-            <span className={styles.logo}>Vínculos</span>
-            <h1 className={styles.title}>tu boda, perfectamente imaginada</h1>
-            <h3>bodas de lujo</h3>
-            <Boton fill="main">Continuar</Boton>
-            <Boton fill="ghost">Contáctanos</Boton>
+            <div className={styles.heroFloat}>
+                <span className={styles.logo}>Vínculos</span>
+                <h1 className={styles.title}>tu boda, <br/>perfectamente imaginada</h1>
+                <h3 className={styles.subtitle}>bodas de lujo</h3>
+                <Boton fill="main" className={styles.button}>Continuar</Boton>
+                <Boton fill="ghost" className={styles.button}>Contáctanos</Boton>
+            </div>
         </div>
     );
 }
