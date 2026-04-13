6c1f3c78ff639ca771f5589e2777aafa4f80e9fa
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-12 22:24:35 -0400

    Format contact section, random stylistic updates

--- src/secciones/Contacto.tsx
+++ src/secciones/Contacto.tsx
@@ -1,14 +1,19 @@
 import styles from './Contacto.module.scss';
+import Boton from "../componentes/Boton.tsx";
+import {useRef} from "react";
 
 export default function Contacto() {
+    const formRef = useRef<HTMLFormElement | null>(null);
+
     return (<article className={styles.contacto}>
         <header className={styles.header}>Contáctanos</header>
-        <form action="" method="POST">
-            <input type="text" name="" id="" placeholder="nombre"/>
-            <input type="email" name="" id="" placeholder="correo electronico"/>
-            <input type="tel" name="" id="" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="teléfono"/>
-            <textarea name="" id="" cols={30} rows={10}></textarea>
-            <input type="submit" value="Submit"/>
+        <form action="" method="POST" className={styles.form} ref={formRef} onSubmit={(e) => { e.preventDefault(); alert("guau!") }}>
+            <input type="text" name="" id="" placeholder="nombre" />
+            <input type="email" name="" id="" placeholder="correo electronico" required />
+            <input type="tel" name="" id="" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="teléfono" onInvalid={(e) => (e.target as HTMLInputElement).setCustomValidity("Enter a phone number, e.g. 123-456-7890")}
+                   onChange={(e) => (e.target as HTMLInputElement).setCustomValidity("")} required />
+            <textarea name="" id="" maxLength={300} placeholder="notas"></textarea>
+            <Boton fill="main" className={styles.button} onclick={() => formRef.current?.requestSubmit()}>Entregar</Boton>
         </form>
     </article>);
 }
