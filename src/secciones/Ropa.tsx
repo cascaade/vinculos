f85e07c346f74542c41a4589dba4e90a2412d344
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-19 23:09:28 -0400

    Remove extra heading

--- src/secciones/Ropa.tsx
+++ src/secciones/Ropa.tsx
@@ -3,7 +3,6 @@ import GaleriaDeImagenes from "../plano/GaleriaDeImagenes.tsx";
 
 export default function Ropa() {
     return (<article className={styles.ropa}>
-        <h3 className="header-title">Ropa</h3>
         <header className={`${styles.header} standard-header`}>
             <h3 className="header-title">Ropa</h3>
             <h4 className="header-body">encontre tu vestido ideal</h4>
