d44d1a5cbcb54dcdeaa772566070423b68213610
Owen <189063419+cascaade@users.noreply.github.com>
2026-04-24 09:55:20 -0400

    Quotes

--- src/plano/Cita.tsx
+++ src/plano/Cita.tsx
@@ -1,3 +1,20 @@
-export default function Cita() {
-    return (<></>)
+import styles from './Cita.module.scss';
+import type {ReactNode} from "react";
+
+export type CitaProps = {
+    children?: ReactNode;
+    name?: string;
+}
+
+export default function Cita({ children, name }: CitaProps) {
+    return (<div className={styles.cita}>
+        <div className={`standard-content ${styles.container}`}>
+            <span className={styles.mark}>&ldquo;</span>
+            <div className={styles.content}>
+                <blockquote className={styles.quote}>{children}</blockquote>
+                <hr/>
+                <span className={styles.name}>{name}</span>
+            </div>
+        </div>
+    </div>);
 }
